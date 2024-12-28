e7902638b7816221d99cc03f9d76ec46f78fa979
TalhaMemisoglu <talhamemisoglu0@gmail.com>
2024-12-28 20:44:58 +0300

    hotfix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -103,11 +103,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PWD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "Ell]@-qPY@HFH+vP",
+        "HOST": "34.116.249.15",
+        "PORT": "5432",
     }
 }
 
