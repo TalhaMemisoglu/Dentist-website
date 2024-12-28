2883b812cd0fada34d48850ce2bcf3a9623de128
TalhaMemisoglu <talhamemisoglu0@gmail.com>
2024-12-29 01:37:16 +0300

    local ip

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "./constants";
 
 // sample JavaScript code snippet
-const apiUrl = "http://dentist-website-production.up.railway.app/";
+const apiUrl = "http://127.0.0.1:8000";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
