1835778fb7dd706ea80fabe13e56a30ba5e8d476
TalhaMemisoglu <talhamemisoglu0@gmail.com>
2024-12-28 17:56:42 +0300

    for trying something

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "./constants";
 
-const apiURL = "/choreo-apis/dentistwebsite/backend/v1"
+const apiURL = "/choreo-apis/backend-2052237686:8000"
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL ? import.meta.envVITE_API_URL : apiURL
