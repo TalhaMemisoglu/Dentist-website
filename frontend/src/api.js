b75399475b0270b2a6a9ad90b0690b04f93b71dc
TalhaMemisoglu <talhamemisoglu0@gmail.com>
2024-12-28 18:02:23 +0300

    hotfix

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,7 +4,7 @@ import { ACCESS_TOKEN, REFRESH_TOKEN } from "./constants";
 const apiURL = "/choreo-apis/backend-2052237686:8000"
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL ? import.meta.envVITE_API_URL : apiURL
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiURL
 });
 
 let isRefreshing = false;
