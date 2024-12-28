a5d1a4bb87ebc1424d6afba924fa20ca3419398c
TalhaMemisoglu <talhamemisoglu0@gmail.com>
2024-12-29 01:49:54 +0300

    url changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({   //Will send the request to the backend server
   server: {
     proxy: {
       '/api': { // This matches any request starting with `/api`
-        target: 'http://127.0.0.1:8000', // Your backend URL
+        target: 'https://dentist-website-production.up.railway.app', // Your backend URL
         changeOrigin: true,
         secure: false,
       },
